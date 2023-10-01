0df5c29a3e6e7753bb3684365f6789a69aca6d68
Dr. David A. Kunz <david.kunz@sap.com>
2023-10-01 12:31:55 +0200

    .

--- README.md
+++ README.md
@@ -17,8 +17,34 @@ vim.keymap.set('v', '<leader>]', ':Gen<CR>')
 vim.keymap.set('n', '<leader>]', ':Gen<CR>')
 ```
 
-You can also directly invoke one of the [predefined prompts](./lua/gen/prompts.lua):
+You can also directly invoke it with one of the [predefined prompts](./lua/gen/prompts.lua):
 
 ```lua
 vim.keymap.set('v', '<leader>]', ':Gen Enhance_Grammar<CR>')
 ```
+
+## Options
+
+All prompts are defined in `require('gen').prompts`, you can enhance or modify the prompts.
+
+```lua
+table.insert(require('gen').prompts, {
+    Elaborate_Text = {
+        prompt = "Elaborate the following text: $text",
+        replace = true
+    },
+    Fix_Code = {
+        prompt = "Fix the following code. Only ouput the result in format ```$filetype\n...\n```:\n\n```$filetype\n$text\n```",
+        replace = true,
+        extract = "```$filetype\n(.-)```"
+    }
+})
+```
+
+You can use the following properties per prompt:
+
+- `prompt`: Prompt which can use the following placeholders:
+   - `$text`: Visually selected text
+   - `$filetype`: Filetype of the buffer (e.g. `javascript`)
+- `replace`: Shall the selected text be replaced?
+- `extract`: Regular expression used to extract the generated result
--- lua/gen/prompts.lua
+++ lua/gen/prompts.lua
@@ -1,25 +1,25 @@
 return {
     Generate = {prompt = "$input1", replace = true},
-    Summarize = {prompt = "Summarize the following text:\n\n```\n$text\n```"},
-    Ask = {prompt = "Regarding the following text, $input1:\n\n```\n$text\n```"},
+    Summarize = {prompt = "Summarize the following text: $text"},
+    Ask = {prompt = "Regarding the following text, $input1: $text"},
     Enhance_Grammar = {
-        prompt = "Enhance the grammar and spelling in the following text:\n\n```\n$text\n```",
+        prompt = "Enhance the grammar and spelling in the following text: $text",
         replace = true
     },
     Enhance_Wording = {
-        prompt = "Enhance the wording in the following text:\n\n```\n$text\n```",
+        prompt = "Enhance the wording in the following text: $text",
         replace = true
     },
     Make_Concise = {
-        prompt = "Make the following text as simple and concise as possible:\n\n```\n$text\n```",
+        prompt = "Make the following text as simple and concise as possible: $text",
         replace = true
     },
     Make_List = {
-        prompt = "Render the following text as a markdown list:\n\n```\n$text\n```",
+        prompt = "Render the following text as a markdown list: $text",
         replace = true
     },
     Make_Table = {
-        prompt = "Render the following text as a markdown table:\n\n```\n$text\n```",
+        prompt = "Render the following text as a markdown table: $text",
         replace = true
     },
     Review_Code = {
