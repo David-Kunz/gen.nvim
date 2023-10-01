401b45dcc677642c4da4392074c6294cab97daa0
Dr. David A. Kunz <david.kunz@sap.com>
2023-10-01 09:21:15 +0200

    .

--- lua/gen/init.lua
+++ lua/gen/init.lua
@@ -18,7 +18,7 @@ local function trim_table(tbl)
     return tbl
 end
 
-M.run_llm = function(opts)
+M.exec = function(opts)
     pcall(io.popen, 'ollama serve > /dev/null 2>&1 &')
     curr_buffer = vim.fn.bufnr('%')
     start_pos = vim.fn.getpos("'<")
@@ -32,7 +32,21 @@ M.run_llm = function(opts)
                                                            end_pos[3] - 1, {}),
                                  '\n')
     local text = vim.fn.shellescape(lines)
-    local instruction = string.gsub(opts.prompt, "%$text", content)
+
+    local function substitute_placeholders(input)
+        if not input then return end
+        local text = input
+        text = string.gsub(text, "%$text", content)
+        text = string.gsub(text, "%$filetype", vim.bo.filetype)
+        if string.find(text, "$input1") then
+            local input1 = vim.fn.input("input1: ")
+            text = string.gsub(text, "%$input1", input1)
+        end
+        return text
+    end
+
+    local instruction = substitute_placeholders(opts.prompt)
+    local extractor = substitute_placeholders(opts.extract)
     local cmd = 'ollama run mistral:instruct """' .. instruction .. '"""'
     if result_buffer then vim.cmd('bd' .. result_buffer) end
     -- vim.cmd('vs enew')
@@ -41,12 +55,26 @@ M.run_llm = function(opts)
     local row = math.floor((vim.o.lines - height) / 2)
     local col = math.floor((vim.o.columns - width) / 2)
 
+    local cursor = vim.api.nvim_win_get_cursor(0)
+    local new_win_width = vim.api.nvim_win_get_width(0)
+    local win_height = vim.api.nvim_win_get_height(0)
+
+    local middle_row = win_height / 2
+
+    local new_win_height = math.floor(win_height / 2)
+    local new_win_row
+    if cursor[1] <= middle_row then
+        new_win_row = 5
+    else
+        new_win_row = -5 - new_win_height
+    end
+
     local win_opts = {
-        relative = 'editor',
-        width = width,
-        height = height,
-        row = row,
-        col = col,
+        relative = 'cursor',
+        width = new_win_width,
+        height = new_win_height,
+        row = new_win_row,
+        col = 0,
         style = 'minimal',
         border = 'single'
     }
@@ -65,6 +93,14 @@ M.run_llm = function(opts)
         end,
         on_exit = function(a, b)
             if b == 0 and opts.replace then
+                if extractor then
+                    local extracted = result_string:match(extractor)
+                    if not extracted then
+                        vim.cmd('bd ' .. result_buffer)
+                        return
+                    end
+                    lines = vim.split(extracted, '\n', true)
+                end
                 lines = trim_table(lines)
                 vim.api.nvim_buf_set_text(curr_buffer, start_pos[2] - 1,
                                           start_pos[3] - 1, end_pos[2] - 1,
@@ -82,29 +118,44 @@ M.run_llm = function(opts)
 end
 
 M.prompts = {
-    Summarize = {prompt = "Summarize the following text: $text"},
-    Fix_Text = {
-        prompt = "Fix the grammar and spelling in the following text: $text",
+    Summarize = {prompt = "Summarize the following text:\n\n```\n$text\n```"},
+    Ask = {prompt = "Regarding the following text, $input1:\n\n```\n$text\n```"},
+    Enhance_Grammar = {
+        prompt = "Enhance the grammar and spelling in the following text:\n\n```\n$text\n```",
+        replace = true
+    },
+    Enhance_Wording = {
+        prompt = "Enhance the wording in the following text:\n\n```\n$text\n```",
         replace = true
     },
     Make_Concise = {
-        prompt = "Make the following text as simple and concise as possible: $text",
+        prompt = "Make the following text as simple and concise as possible:\n\n```\n$text\n```",
         replace = true
     },
-    Make_Markdown_List = {
-        prompt = "Render the following text as a markdown list: $text",
+    Make_List = {
+        prompt = "Render the following text as a markdown list:\n\n```\n$text\n```",
         replace = true
     },
-    Format_Markdown_Table = {
-        prompt = "Fix and format the following markdown table: $text",
+    Make_Table = {
+        prompt = "Render the following text as a markdown table:\n\n```\n$text\n```",
         replace = true
     },
     Review_Code = {
-        prompt = "Review the following code and make suggestions: $text"
+        prompt = "Review the following code and make concise suggestions:\n\n```$filetype\n$text\n```"
+    },
+    Enhance_Code = {
+        prompt = "Enhance the following code, only ouput the result in format ```$filetype\n...\n```:\n\n```$filetype\n$text\n```",
+        replace = true,
+        extract = "```$filetype\n(.-)```"
+    },
+    Change_Code = {
+        prompt = "Regarding the following code, $input1, only ouput the result in format ```$filetype\n...\n```:\n\n```$filetype\n$text\n```",
+        replace = true,
+        extract = "```$filetype\n(.-)```"
     }
 }
 
-vim.api.nvim_create_user_command('Gen', function()
+function select_prompt(cb)
     local promptKeys = {}
     for key, _ in pairs(M.prompts) do table.insert(promptKeys, key) end
     vim.ui.select(promptKeys, {
@@ -112,8 +163,12 @@ vim.api.nvim_create_user_command('Gen', function()
         format_item = function(item)
             return table.concat(vim.split(item, '_'), ' ')
         end
-    }, function(item, idx) M.run_llm(M.prompts[item]) end)
+    }, function(item, idx) cb(item) end)
+end
 
-end, {range = true})
+vim.api.nvim_create_user_command('Gen', function()
+    select_prompt(function(item) M.exec(M.prompts[item]) end)
+
+end, {range = true, nargs = '?'})
 
 return M
