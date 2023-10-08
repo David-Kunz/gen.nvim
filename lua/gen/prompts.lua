2c5ac7f14f078027e1a7d872b956bcce35f88813
Dr. David A. Kunz <david.kunz@sap.com>
2023-10-08 08:32:11 +0200

    fix: reduce probability for LLM to put quotes around generated text

--- lua/gen/prompts.lua
+++ lua/gen/prompts.lua
@@ -3,19 +3,19 @@ return {
     Summarize = {prompt = "Summarize the following text:\n$text"},
     Ask = {prompt = "Regarding the following text, $input:\n$text"},
     Change = {
-        prompt = "Change the following text, $input:\n$text",
+        prompt = "Change the following text, $input, just output the final text without additional quotes around it:\n$text",
         replace = true
     },
     Enhance_Grammar_Spelling = {
-        prompt = "Modify the following text to improve grammar and spelling:\n$text",
+        prompt = "Modify the following text to improve grammar and spelling, just output the final text without additional quotes around it:\n$text",
         replace = true
     },
     Enhance_Wording = {
-        prompt = "Modify the following text to use better wording:\n$text",
+        prompt = "Modify the following text to use better wording, just output the final text without additional quotes around it:\n$text",
         replace = true
     },
     Make_Concise = {
-        prompt = "Modify the following text to make it as simple and concise as possible:\n$text",
+        prompt = "Modify the following text to make it as simple and concise as possible, just output the final text without additional quotes around it:\n$text",
         replace = true
     },
     Make_List = {
