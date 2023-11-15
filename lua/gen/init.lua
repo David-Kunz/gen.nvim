a8aded7d9b25c35f7c4f4d6f392d8baadf6bf286
Kevin Johnson <kljohnson@verisk.com>
2023-11-15 12:06:54 -0700

    Adds conversation support

--- lua/gen/init.lua
+++ lua/gen/init.lua
@@ -1,4 +1,4 @@
-local prompts = require('gen.prompts')
+local prompts = require("gen.prompts")
 local M = {}
 
 local curr_buffer = nil
@@ -6,7 +6,9 @@ local start_pos = nil
 local end_pos = nil
 
 local function trim_table(tbl)
-    local function is_whitespace(str) return str:match("^%s*$") ~= nil end
+    local function is_whitespace(str)
+        return str:match("^%s*$") ~= nil
+    end
 
     while #tbl > 0 and (tbl[1] == "" or is_whitespace(tbl[1])) do
         table.remove(tbl, 1)
@@ -19,82 +21,50 @@ local function trim_table(tbl)
     return tbl
 end
 
-local function get_window_options()
-
-    local width = math.floor(vim.o.columns * 0.9) -- 90% of the current editor's width
-    local height = math.floor(vim.o.lines * 0.9)
-    local row = math.floor((vim.o.lines - height) / 2)
-    local col = math.floor((vim.o.columns - width) / 2)
-
-    local cursor = vim.api.nvim_win_get_cursor(0)
-    local new_win_width = vim.api.nvim_win_get_width(0)
-    local win_height = vim.api.nvim_win_get_height(0)
-
-    local middle_row = win_height / 2
-
-    local new_win_height = math.floor(win_height / 2)
-    local new_win_row
-    if cursor[1] <= middle_row then
-        new_win_row = 5
-    else
-        new_win_row = -5 - new_win_height
-    end
-
-    return {
-        relative = 'cursor',
-        width = new_win_width,
-        height = new_win_height,
-        row = new_win_row,
-        col = 0,
-        style = 'minimal',
-        border = 'single'
-    }
-end
-
-M.command = 'ollama run $model $prompt'
-M.model = 'mistral:instruct'
+M.command = "ollama run $model $prompt"
+M.model = "mistral:instruct"
 M.container = nil
 M.debugCommand = false
 
-local commandContainer = 'docker exec $container ollama run $model $prompt'
+local commandContainer = "docker exec $container ollama run $model $prompt"
 
 M.exec = function(options)
-    if M.container ~= nil and M.command == 'ollama run $model $prompt' then
+    if M.container ~= nil and M.command == "ollama run $model $prompt" then
         M.command = commandContainer
     end
-    local opts = vim.tbl_deep_extend('force', {
+    local opts = vim.tbl_deep_extend("force", {
         model = M.model,
         command = M.command,
         container = M.container,
         debugCommand = M.debugCommand,
-        win_config = M.win_config
+        win_config = M.win_config,
     }, options)
     if opts.container ~= nil then
-        pcall(io.popen, 'docker start ' .. opts.container)
+        pcall(io.popen, "docker start " .. opts.container)
     else
-        pcall(io.popen, 'ollama serve > /dev/null 2>&1 &')
+        pcall(io.popen, "ollama serve > /dev/null 2>&1 &")
     end
-    curr_buffer = vim.fn.bufnr('%')
+    curr_buffer = vim.fn.bufnr("%")
     local mode = opts.mode or vim.fn.mode()
-    if mode == 'v' or mode == 'V' then
+    if mode == "v" or mode == "V" then
         start_pos = vim.fn.getpos("'<")
         end_pos = vim.fn.getpos("'>")
         end_pos[3] = vim.fn.col("'>") -- in case of `V`, it would be maxcol instead
     else
-        local cursor = vim.fn.getpos('.')
+        local cursor = vim.fn.getpos(".")
         start_pos = cursor
         end_pos = start_pos
     end
 
-    local content = table.concat(vim.api.nvim_buf_get_text(curr_buffer,
-                                                           start_pos[2] - 1,
-                                                           start_pos[3] - 1,
-                                                           end_pos[2] - 1,
-                                                           end_pos[3] - 1, {}),
-                                 '\n')
+    local content = table.concat(
+        vim.api.nvim_buf_get_text(curr_buffer, start_pos[2] - 1, start_pos[3] - 1, end_pos[2] - 1, end_pos[3] - 1, {}),
+        "\n"
+    )
 
     local function substitute_placeholders(input)
-        if not input then return end
+        if not input then
+            return
+        end
         local text = input
         if string.find(text, "%$input") then
             local answer = vim.fn.input("Prompt: ")
@@ -119,10 +89,10 @@ M.exec = function(options)
     local prompt = opts.prompt
 
     if type(prompt) == "function" then
-        prompt = prompt({content = content, filetype = vim.bo.filetype})
+        prompt = prompt({ content = content, filetype = vim.bo.filetype })
     end
 
-    prompt = vim.fn.shellescape(substitute_placeholders(prompt))
+    prompt = substitute_placeholders(prompt)
     local extractor = substitute_placeholders(opts.extract)
     local cmd = opts.command
     prompt = string.gsub(prompt, "%%", "%%%%")
@@ -131,71 +101,110 @@ M.exec = function(options)
     if opts.container ~= nil then
         cmd = string.gsub(cmd, "%$container", opts.container)
     end
-    if result_buffer then vim.cmd('bd' .. result_buffer) end
-    local win_opts = vim.tbl_deep_extend('force', get_window_options(),
-                                         opts.win_config)
-    result_buffer = vim.api.nvim_create_buf(false, true)
-    vim.api.nvim_buf_set_option(result_buffer, 'filetype', 'markdown')
+    if M.result_buffer == nil then
+        vim.cmd("vnew")
+        M.result_buffer = vim.fn.bufnr("%")
+        M.float_win = vim.fn.win_getid()
+        vim.api.nvim_buf_set_option(M.result_buffer, "filetype", "markdown")
+        vim.api.nvim_win_set_option(M.float_win, "wrap", true)
+    end
 
-    local float_win = vim.api.nvim_open_win(result_buffer, true, win_opts)
-
-    local result_string = ''
+    local result_string = ""
     local lines = {}
     local job_id
+    local data = {
+        model = opts.model,
+        prompt = prompt,
+        stream = false,
+    }
+
+    if M.context then
+        data.context = M.context
+    end
+
+    local json = vim.fn.json_encode(data)
+    cmd = "curl -X POST http://localhost:11434/api/generate -d '" .. json .. "'"
+
+    vim.api.nvim_buf_set_option(M.result_buffer, "modifiable", true)
+    vim.api.nvim_buf_set_lines(
+        M.result_buffer,
+        -1,
+        -1,
+        false,
+        vim.split("```text\n================\n" .. prompt .. "\n================\n```\n", "\n")
+    )
+    vim.api.nvim_buf_set_option(M.result_buffer, "modifiable", false)
+    vim.fn.feedkeys("G")
     job_id = vim.fn.jobstart(cmd, {
         on_stdout = function(_, data, _)
             -- window was closed, so cancel the job
-            if not vim.api.nvim_win_is_valid(float_win) then
+            if not vim.api.nvim_win_is_valid(M.float_win) then
                 vim.fn.jobstop(job_id)
+                vim.cmd("bd " .. M.result_buffer)
+                M.result_buffer = nil
+                M.float_win = nil
                 return
             end
-            result_string = result_string .. table.concat(data, '\n')
-            lines = vim.split(result_string, '\n', true)
-            vim.api.nvim_buf_set_lines(result_buffer, 0, -1, false, lines)
-            vim.api.nvim_win_call(float_win, function()
-                vim.fn.feedkeys('$')
-            end)
+
+            local body = vim.fn.json_decode(table.concat(data, "\n"))
+
+            result_string = result_string .. table.concat(data, "\n")
+            lines = vim.split(result_string, "\n", true)
+            lines = vim.split(body.response .. "\n", "\n")
+            vim.api.nvim_buf_set_option(M.result_buffer, "modifiable", true)
+            vim.api.nvim_buf_set_lines(M.result_buffer, -1, -1, false, lines)
+            vim.api.nvim_buf_set_option(M.result_buffer, "modifiable", false)
+            vim.fn.feedkeys("G")
+            M.context = body.context
         end,
         on_stderr = function(_, data, _)
             if opts.debugCommand then
                 -- window was closed, so cancel the job
-                if not vim.api.nvim_win_is_valid(float_win) then
+                if not vim.api.nvim_win_is_valid(M.float_win) then
                     vim.fn.jobstop(job_id)
                     return
                 end
-                result_string = result_string .. table.concat(data, '\n')
-                lines = vim.split(result_string, '\n', true)
-                vim.api.nvim_buf_set_lines(result_buffer, 0, -1, false, lines)
-                vim.api.nvim_win_call(float_win, function()
-                    vim.fn.feedkeys('$')
-                end)
+                result_string = result_string .. table.concat(data, "\n")
+                lines = vim.split(result_string, "\n", true)
+                vim.api.nvim_buf_set_lines(M.result_buffer, -1, -1, false, lines)
             end
         end,
-        stderr_buffered = opts.debugCommand,
         on_exit = function(a, b)
             if b == 0 and opts.replace then
                 if extractor then
                     local extracted = result_string:match(extractor)
                     if not extracted then
-                        vim.cmd('bd ' .. result_buffer)
+                        vim.cmd("bd " .. M.result_buffer)
+                        M.result_buffer = nil
+                        M.float_win = nil
                         return
                     end
-                    lines = vim.split(extracted, '\n', true)
+                    lines = vim.split(extracted, "\n", true)
                 end
                 lines = trim_table(lines)
-                vim.api.nvim_buf_set_text(curr_buffer, start_pos[2] - 1,
-                                          start_pos[3] - 1, end_pos[2] - 1,
-                                          end_pos[3] - 1, lines)
-                vim.cmd('bd ' .. result_buffer)
+                vim.api.nvim_buf_set_text(
+                    curr_buffer,
+                    start_pos[2] - 1,
+                    start_pos[3] - 1,
+                    end_pos[2] - 1,
+                    end_pos[3] - 1,
+                    lines
+                )
+                vim.cmd("bd " .. M.result_buffer)
+                M.result_buffer = nil
+                M.float_win = nil
             end
-        end
+        end,
     })
-    vim.keymap.set('n', '<esc>', function() vim.fn.jobstop(job_id) end,
-                   {buffer = result_buffer})
+    vim.keymap.set("n", "<esc>", function()
+        vim.fn.jobstop(job_id)
+    end, { buffer = M.result_buffer })
 
-    vim.api.nvim_buf_attach(result_buffer, false,
-                            {on_detach = function() result_buffer = nil end})
-
+    vim.api.nvim_buf_attach(M.result_buffer, false, {
+        on_detach = function()
+            M.result_buffer = nil
+        end,
+    })
 end
 
 M.win_config = {}
@@ -203,41 +212,46 @@ M.win_config = {}
 M.prompts = prompts
 function select_prompt(cb)
     local promptKeys = {}
-    for key, _ in pairs(M.prompts) do table.insert(promptKeys, key) end
+    for key, _ in pairs(M.prompts) do
+        table.insert(promptKeys, key)
+    end
     table.sort(promptKeys)
     vim.ui.select(promptKeys, {
-        prompt = 'Prompt:',
+        prompt = "Prompt:",
         format_item = function(item)
-            return table.concat(vim.split(item, '_'), ' ')
-        end
-    }, function(item, idx) cb(item) end)
+            return table.concat(vim.split(item, "_"), " ")
+        end,
+    }, function(item, idx)
+        cb(item)
+    end)
 end
 
-vim.api.nvim_create_user_command('Gen', function(arg)
+vim.api.nvim_create_user_command("Gen", function(arg)
     local mode
     if arg.range == 0 then
-        mode = 'n'
+        mode = "n"
     else
-        mode = 'v'
+        mode = "v"
     end
-    if arg.args ~= '' then
+    if arg.args ~= "" then
         local prompt = M.prompts[arg.args]
         if not prompt then
             print("Invalid prompt '" .. arg.args .. "'")
             return
         end
-        p = vim.tbl_deep_extend('force', {mode = mode}, prompt)
+        p = vim.tbl_deep_extend("force", { mode = mode }, prompt)
         return M.exec(p)
     end
     select_prompt(function(item)
-        if not item then return end
-        p = vim.tbl_deep_extend('force', {mode = mode}, M.prompts[item])
+        if not item then
+            return
+        end
+        p = vim.tbl_deep_extend("force", { mode = mode }, M.prompts[item])
         M.exec(p)
     end)
-
 end, {
     range = true,
-    nargs = '?',
+    nargs = "?",
     complete = function(ArgLead, CmdLine, CursorPos)
         local promptKeys = {}
         for key, _ in pairs(M.prompts) do
@@ -247,7 +261,7 @@ end, {
         end
         table.sort(promptKeys)
         return promptKeys
-    end
+    end,
 })
 
 return M
