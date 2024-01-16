c404e217e1e0deb1f1aaa9e9ff61fc3e3c23efbe
Dung Duc Huynh (Kaka) <870029+jellydn@users.noreply.github.com>
2024-01-16 08:25:06 +0800

    refactor: show spinner on its buffer and window (#1)

--- lua/gen/spinner.lua
+++ lua/gen/spinner.lua
@@ -1,49 +1,78 @@
 local M = {}
 
-local spinner = {
-    "⠋",
-    "⠙",
-    "⠹",
-    "⠸",
-    "⠼",
-    "⠴",
-    "⠦",
-    "⠧",
-    "⠇",
-    "⠏",
+-- User configuration section
+local config = {
+    -- Show notification when done.
+    -- Set to false to disable.
+    show_notification = true,
+    -- Name of the plugin.
+    plugin = "Gen.nvim",
+    -- Spinner frames.
+    spinner_frames = {
+        "⠋",
+        "⠙",
+        "⠹",
+        "⠸",
+        "⠼",
+        "⠴",
+        "⠦",
+        "⠧",
+        "⠇",
+        "⠏",
+    },
 }
 
-local spinner_index = 1 -- to keep track of the spinner frame
-local spinner_shown = false -- to keep track if spinner is already shown
-local spinner_timer = nil -- timer for updating the spinner
--- NOTE:  This is configured locally rather than as a plugin for simplicity.
-local interval = 300 -- spinner update interval in ms
+local spinner_index = 1
+local spinner_timer = nil
+local spinner_buf = nil
+local spinner_win = nil
 
-local function update_spinner()
-    local spinner_frame = spinner[spinner_index]
-    spinner_index = spinner_index % #spinner + 1
-    print(spinner_frame)
+--- Show a spinner at the specified position.
+---@param position? table
+function M.show_spinner(position)
+    -- Default position the top right corner
+    local default_position = {
+        relative = "editor",
+        width = 1,
+        height = 1,
+        col = vim.o.columns - 1,
+        row = 0,
+    }
+    local options = position or default_position
+    options.style = "minimal"
+
+    -- Create buffer and window for the spinner
+    spinner_buf = vim.api.nvim_create_buf(false, true)
+    spinner_win = vim.api.nvim_open_win(spinner_buf, false, options)
+
+    -- Set up timer and update spinner
+    spinner_timer = vim.loop.new_timer()
+    spinner_timer:start(
+        0,
+        100,
+        vim.schedule_wrap(function()
+            vim.api.nvim_buf_set_lines(spinner_buf, 0, -1, false, { config.spinner_frames[spinner_index] })
+            spinner_index = spinner_index % #config.spinner_frames + 1
+        end)
+    )
 end
 
-M.show_spinner = function()
-    if not spinner_shown then
-        vim.notify("Generating...", vim.log.levels.INFO, { title = "Gen.nvim" })
-        spinner_timer = vim.loop.new_timer()
-        spinner_timer:start(interval, interval, vim.schedule_wrap(update_spinner))
-        spinner_shown = true
-    end
-end
-
-M.hide_spinner = function()
-    if spinner_shown then
-        vim.notify("Done!", vim.log.levels.INFO, { title = "Gen.nvim" })
-        spinner_shown = false
-        if spinner_timer == nil then
-            return
-        end
+--- Hide the spinner.
+function M.hide_spinner()
+    if spinner_timer then
         spinner_timer:stop()
         spinner_timer:close()
         spinner_timer = nil
+        if spinner_win then
+            vim.api.nvim_win_close(spinner_win, true)
+        end
+        if spinner_buf then
+            vim.api.nvim_buf_delete(spinner_buf, { force = true })
+        end
+
+        if config.show_notification then
+            vim.notify("Done!", vim.log.levels.INFO, { title = config.plugin })
+        end
     end
 end
 
